8414aab8783b6126b5b73f527ff0de7c42fa8500
Mika-grd <miguela.betancourtc@uqvirtual.edu.co>
2025-04-07 22:45:13 -0500

    Interface del crud y cuentas para implementar el builder

--- src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/ICrud.java
+++ src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/ICrud.java
@@ -1,4 +1,8 @@
 package co.edu.uniquindio.poo.proyecto_final_programacion_2.model.base;
 
 public interface ICrud {
+    public <T> String agregarObjeto(T objeto);
+    public <T> String actualizarObjeto(T objeto);
+    public <T> String eliminarObjeto(T objeto);
+    public <T> String buscarObjeto(T objeto);
 }
