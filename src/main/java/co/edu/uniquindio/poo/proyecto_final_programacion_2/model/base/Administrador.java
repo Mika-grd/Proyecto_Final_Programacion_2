d43728208dff2f86d4b386d50bb2259e259e2642
Mika-grd <miguela.betancourtc@uqvirtual.edu.co>
2025-04-08 19:20:35 -0500

    Correccion metofos genericos

--- src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/Administrador.java
+++ src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/Administrador.java
@@ -3,28 +3,31 @@ package co.edu.uniquindio.poo.proyecto_final_programacion_2.model.base;
 import java.util.LinkedList;
 
 public class Administrador extends Persona implements IAdmin {
+
     public Administrador(String nombre, String id, String correo, String telefono) {
         super(nombre, id, correo, telefono);
+
     }
 
 
     @Override
     public <T> String agregarObjeto(T objeto, LinkedList<T> listaObjetos) {
-        return agregarObjeto(objeto, listaObjetos);
+        return BilleteraVirtual.getInstance().agregarObjeto(objeto, listaObjetos);
     }
 
     @Override
     public <T> String editarObjeto(T objeto, T objetoNuevo, LinkedList<T> listaObjetos) {
-        return editarObjeto(objeto, objetoNuevo, listaObjetos);
+        return BilleteraVirtual.getInstance().editarObjeto(objeto, objetoNuevo, listaObjetos);
     }
 
     @Override
     public <T> String eliminarObjeto(T objeto, LinkedList<T> listaObjetos) {
-        return eliminarObjeto(objeto, listaObjetos);
+        return BilleteraVirtual.getInstance().eliminarObjeto(objeto, listaObjetos);
     }
 
     @Override
     public <T> Object buscarObjeto(T id, LinkedList<T> listaObjetos) {
-        return buscarObjeto(id, listaObjetos);
+
+        return BilleteraVirtual.getInstance().buscarObjeto(id, listaObjetos);
     }
 }
--- src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/builder/CuentasBuilder.java
+++ src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/builder/CuentasBuilder.java
@@ -1,4 +1,5 @@
 package co.edu.uniquindio.poo.proyecto_final_programacion_2.model.builder;
 
 public interface CuentasBuilder {
+
 }
