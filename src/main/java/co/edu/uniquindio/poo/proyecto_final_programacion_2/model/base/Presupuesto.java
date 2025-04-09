75e3660b58721bbc0621abd69a18d410e995d3fd
Juan. Maldonado <juanmaldo0618@gmail.com>
2025-04-08 19:15:40 -0500

    Crud generico

--- src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/Presupuesto.java
+++ src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/Presupuesto.java
@@ -1,8 +1,55 @@
 package co.edu.uniquindio.poo.proyecto_final_programacion_2.model.base;
 
 public class Presupuesto {
-    private String idPresupuesto;
+    private String id;
     private String nombre;
     private int montoActual;
     private int montoGastado;
+
+    ///////Constructor
+    public Presupuesto(String id, String nombre, int montoActual, int montoGastado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.montoActual = montoActual;
+        this.montoGastado = montoGastado;
+    }
+
+
+
+
+
+
+
+    ////////Setters & getters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getMontoActual() {
+        return montoActual;
+    }
+
+    public void setMontoActual(int montoActual) {
+        this.montoActual = montoActual;
+    }
+
+    public int getMontoGastado() {
+        return montoGastado;
+    }
+
+    public void setMontoGastado(int montoGastado) {
+        this.montoGastado = montoGastado;
+    }
 }
--- src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/Usuario.java
+++ src/main/java/co/edu/uniquindio/poo/proyecto_final_programacion_2/model/base/Usuario.java
@@ -1,10 +1,53 @@
 package co.edu.uniquindio.poo.proyecto_final_programacion_2.model.base;
 
-public class Usuario extends Persona {
+import java.util.LinkedList;
+
+public class Usuario extends Persona implements IUsuario{
+
     private double saldo;
 
+    ///////Constructor
     public Usuario(String nombre, String id, String correo, String telefono, double saldo) {
         super(nombre, id, correo, telefono);
+        this.saldo = saldo;
+    }
+
+
+
+    /// Crud generico
+    @Override
+    public <T> String agregarObjeto(T objeto, LinkedList<T> listaObjetos) {
+        return agregarObjeto(objeto, listaObjetos);
+    }
+
+    @Override
+    public <T> String editarObjeto(T objeto, T objetoNuevo, LinkedList<T> listaObjetos) {
+        return editarObjeto(objeto, objetoNuevo, listaObjetos);
+    }
+
+    @Override
+    public <T> String eliminarObjeto(T objeto, LinkedList<T> listaObjetos) {
+        return eliminarObjeto(objeto, listaObjetos);
+    }
+
+    @Override
+    public <T> Object buscarObjeto(T id, LinkedList<T> listaObjetos) {
+        return buscarObjeto(id, listaObjetos);
+    }
+
+
+
+
+
+
+
+
+    ////Setters & getters
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 }
